b813e8e4ee547e5ab329f355bde924b9e105845a
Luis Henrique Ries <prof.ries@gmail.com>
2023-03-23 21:33:01 -0300

    Tarefa 2

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ const express = require('express')
 const app = express();
 const PORTA = 3000;
 
-app.get("/hello", (req, res) => { 
-    res.send("Hello World");
+app.get("/api/produtos", (req, res) => { 
+    res.send("Lista de Produtos");
+})
+
+app.get("/api/produtos/:id", (req, res) => { 
+    const id = req.params.id;
+    res.send(`Buscar pelo id: ${id}`);
+})
+
+app.post("/api/produtos", (req, res) => { 
+    res.send("Cadastro de Produtos");
+})
+
+app.put("/api/produtos/:id", (req, res) => { 
+    const id = req.params.id;
+    res.send(`Atualizando o produto ${id}`);
+})
+
+app.delete("/api/produtos/:id", (req, res) => { 
+    const id = req.params.id;
+    res.send(`Deletando o produto ${id}`);
 })
 
 app.listen(PORTA, () => {
